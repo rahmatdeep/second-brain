eab737e2a0663647e80cb48ca0ab6cf9f256c8f2
rahmatdeep <rahmatdeep@gmail.com>
2024-11-30 15:20:46 +0530

    update modal background

--- frontend/src/components/CreateContentModel.tsx
+++ frontend/src/components/CreateContentModel.tsx
@@ -1,6 +1,12 @@
+import { useRef, useState } from "react";
 import { CrossIcon } from "../icons/CrossIcon";
 import { Button } from "./Button";
 import { Input } from "./Input";
+
+enum ContentType {
+  Youtube = "youtube",
+  Twitter = "twitter",
+}
 
 export function CreateContentModel({
   open,
@@ -9,25 +15,57 @@ export function CreateContentModel({
   open: boolean;
   onClose: () => void;
 }) {
+  const titleRef = useRef<HTMLInputElement>(null);
+  const linkRef = useRef<HTMLInputElement>(null);
+  const [type, setType] = useState(ContentType.Youtube);
+
+  function addContent() {
+    const title = titleRef.current?.value;
+    const link = linkRef.current?.value;
+  }
+
   return (
     <div>
       {open && (
-        <div className="w-screen h-screen bg-slate-500 fixed top-0 left-0 opacity-60 flex justify-center">
-          <div className="flex flex-col justify-center">
-            <span className="bg-white opacity-100 p-4 rounded">
-              <div className="flex justify-end">
-                <button onClick={onClose}>
-                  <CrossIcon />
-                </button>
-              </div>
-              <div>
-                <Input placeHolder="Title" />
-                <Input placeHolder="link" />
-              </div>
-              <div className="flex justify-center">
-                <Button variant="primary" text="Submit" />
-              </div>
-            </span>
+        <div>
+          <div className="w-screen h-screen bg-slate-500 fixed top-0 left-0 opacity-60 flex justify-center"></div>
+          <div className="w-screen h-screen fixed top-0 left-0 flex justify-center">
+            <div className="flex flex-col justify-center">
+              <span className="bg-white opacity-100 p-4 rounded">
+                <div className="flex justify-end">
+                  <button className="text-purple-500" onClick={onClose}>
+                    <CrossIcon />
+                  </button>
+                </div>
+                <div>
+                  <Input placeHolder="Title" reference={titleRef} />
+                  <Input placeHolder="Link" reference={linkRef} />
+                  <div className="flex justify-evenly pb-2 px-2">
+                    <Button
+                      text="Youtube"
+                      variant={
+                        type === ContentType.Youtube ? "primary" : "secondary"
+                      }
+                      onClick={() => setType(ContentType.Youtube)}
+                    />
+                    <Button
+                      text="Twitter"
+                      variant={
+                        type === ContentType.Twitter ? "primary" : "secondary"
+                      }
+                      onClick={() => setType(ContentType.Twitter)}
+                    />
+                  </div>
+                </div>
+                <div className="flex justify-center">
+                  <Button
+                    variant="primary"
+                    text="Submit"
+                    onClick={addContent}
+                  />
+                </div>
+              </span>
+            </div>
           </div>
         </div>
       )}
